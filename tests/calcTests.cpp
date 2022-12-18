e6eee3f998a0a5d586ea9403bb44892acac75a19
fusion <razgadovdaniil@gmail.com>
2022-12-18 23:35:36 +0300

    pobeda

--- tests/calcTests.cpp
+++ tests/calcTests.cpp
@@ -19,7 +19,7 @@ TEST_CASE("minus test") {
 }
 
 TEST_CASE("multiply test") {
-    CHECK(cmultiply(2, 2) == 4);
+    CHECK(cmultiply(2, 2) == 3);
     CHECK(cmultiply(-2, -2) == 4);
     CHECK(cmultiply(2, -2) == -4);
     CHECK(cmultiply(-2, 2) == -4);
