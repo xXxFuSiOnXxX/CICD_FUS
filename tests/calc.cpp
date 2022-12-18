609bda5cbbf083720fbec08940ad23bcc742f600
fusion <razgadovdaniil@gmail.com>
2022-12-18 16:29:08 +0300

    pobeda

---
+++ tests/calc.cpp
@@ -0,0 +1,21 @@
+#include "calc.h"
+
+double cplus(double x, double y) {
+    return x + y;
+}
+
+double cbrokePlus(double x, double y) {
+    return x + y + 1;
+}
+
+double cminus(double x, double y) {
+    return x - y;
+}
+
+double cmultiply(double x, double y) {
+    return x * y;
+}
+
+double cdivide(double x, double y) {
+    return x / y;
+}
