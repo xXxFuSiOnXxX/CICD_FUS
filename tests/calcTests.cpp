2aca3d994b3e2ac687fa2c3078453ce6fcca8d14
fusion <razgadovdaniil@gmail.com>
2022-12-18 16:34:48 +0300

    pobeda

--- tests/calcTests.cpp
+++ tests/calcTests.cpp
@@ -5,30 +5,29 @@
 
 
 TEST_CASE("plus test") {
-    SUBCASE("++") CHECK(cplus(1, 1) == 2);
-    SUBCASE("--") CHECK(cplus(-1, -1) == -2);
-    SUBCASE("+-") CHECK(cplus(1, -1) == 0);
-    SUBCASE("-+") CHECK(cplus(-1, 1) == 0);
-    //SUBCASE("broke") CHECK(cbrokePlus(1, 1) == 15);
+    CHECK(cplus(1, 1) == 2);
+    CHECK(cplus(-1, -1) == -2);
+    CHECK(cplus(1, -1) == 0);
+    CHECK(cplus(-1, 1) == 0);
 }
 
 TEST_CASE("minus test") {
-    SUBCASE("++") CHECK(cminus(1, 1) == 0);
-    SUBCASE("--") CHECK(cminus(-1, -1) == 0);
-    SUBCASE("+-") CHECK(cminus(1, -1) == 2);
-    SUBCASE("-+") CHECK(cminus(-1, 1) == -2);
+    CHECK(cminus(1, 1) == 0);
+    CHECK(cminus(-1, -1) == 0);
+    CHECK(cminus(1, -1) == 2);
+    CHECK(cminus(-1, 1) == -2);
 }
 
 TEST_CASE("multiply test") {
-    SUBCASE("++") CHECK(cmultiply(2, 2) == 3);
-    SUBCASE("--") CHECK(cmultiply(-2, -2) == 4);
-    SUBCASE("+-") CHECK(cmultiply(2, -2) == -4);
-    SUBCASE("-+") CHECK(cmultiply(-2, 2) == -4);
+    CHECK(cmultiply(2, 2) == 4);
+    CHECK(cmultiply(-2, -2) == 4);
+    CHECK(cmultiply(2, -2) == -4);
+    CHECK(cmultiply(-2, 2) == -4);
 }
 
 TEST_CASE("divide test") {
-    SUBCASE("++") CHECK(cdivide(2, 2) == 1);
-    SUBCASE("--") CHECK(cdivide(-2, -2) == 1);
-    SUBCASE("+-") CHECK(cdivide(2, -2) == -1);
-    SUBCASE("-+") CHECK(cdivide(-2, 2) == -1);
+    CHECK(cdivide(2, 2) == 1);
+    CHECK(cdivide(-2, -2) == 1);
+    CHECK(cdivide(2, -2) == -1);
+    CHECK(cdivide(-2, 2) == -1);
 }
